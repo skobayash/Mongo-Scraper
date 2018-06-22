09bfef7b02f435ee981a66b12977f26a5803eaad
sobayashi <sophieobayashi@gmail.com>
2018-06-21 22:04:48 -0700

    added style.css

--- public/app.js
+++ public/app.js
@@ -4,7 +4,10 @@ $.getJSON("/articles", function(data) {
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
     $("#articles").append(
-      "<div class='card'><div class='card-header'><h5>" + data[i].title + "</h5></div><div class='card-body'><p data-id=' " + data[i]._id + "'>" + data[i].excerpt + "<br /><div class='text-center'><a href='" + data[i].link + "' class='btn btn-dark'>View Article</a></div></p></div></div> ")
+      "<div class='card'><div class='card-header'><h5>" + data[i].title + 
+      "</h5></div><div class='card-body'><div class='row'><div class='col'><img src='" + data[i].image + "'></div><div class='col'>" 
+      + "<p data-id='" + data[i]._id + "'>" + data[i].excerpt + "<br /><div class='align-text-bottom'><a href='" 
+      + data[i].link + "' class='btn btn-dark' target='_blank'>View Article</a></div></div></p></div></div></div></div> ")
   }
 });
   
@@ -24,14 +27,10 @@ $.getJSON("/articles", function(data) {
       // With that done, add the note information to the page
       .then(function(data) {
         console.log(data);
-        // The title of the article
-        $("#comments").append("<p class='font-weight-bold'>" + data.title + "</p>");
-        // An input to enter a new title
-        $("#comments").append("<input id='titleinput' name='title' placeholder='Note Title'>");
-        // A textarea to add a new note body
-        $("#comments").append("<textarea id='bodyinput' name='body' placeholder='Comments...'></textarea>");
-        // A button to submit a new note, with the id of the article saved to it
-        $("#comments").append("<button data-id='" + data._id + "' id='savecomment' class='btn btn-dark'>Save</button>");
+        $("#comments").append("<div class='card comment-card'><div class='card-header'><p class='font-weight-bold'>"
+        + data.title + "</p><input id='titleinput' name='title' class='form-control' placeholder='Note Title'></div><div class='card-body'>"
+        + "<textarea class='form-control' rows='5' id='bodyinput' name='body' placeholder='Comments...'></textarea><button data-id='"
+        + data._id + "' id='savecomment' class='btn btn-dark'>Save</button></div></div>");
   
         // If there's a note in the article
         if (data.comment) {
